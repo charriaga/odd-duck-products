0ed4714651564b0d18b5c469da06d696f1bc4046
Charlie Arriaga <arriayaa@gmail.com>
2023-11-07 15:13:47 -0800

    fixed images try 4

--- js/app.js
+++ js/app.js
@@ -17,25 +17,27 @@ function Product(productName, src, alt) {
     this.clicks = 0;
 }
 
-const bag = new Product('bag', './img/bag.jpg', 'r2d2 suitcase');
-const banana = new Product('banana', './img/banana.jpg', 'banana shaped banana slicer');
-const bathroom = new Product('bathroom', './img/bathroom.jpg', 'toilet paper roll stand that doubles as an ipad stand');
-const boots = new Product('boots', './img/boots.jpg', 'open toed rain boots');
-const breakfast = new Product('breakfast', './img/breakfast.jpg', 'a machine that makes eggs, bacon, coffee, and toast all in one');
-const bubblegum = new Product('bubblegum', './img/bubblegum.jpg', 'meatball bubblegum');
-const chair = new Product('chair', './img/chair.jpg', 'plastic chair with the seat bubbling outward');
-const cthulhu = new Product('cthulhu', './img/cthulhu.jpg', 'cthulhu action figure');
-const dogDuck = new Product('dog duck', './img/dog-duck.jpg', 'dog muzzle shaped like a duck bill');
-const dragon = new Product('dragon', './img/dragon.jpg', 'canned dragon meat');
-const pen = new Product('pen', './img/pen.jpg', 'pens with silverware shaped caps');
-const petSweep = new Product('pen sweep', './img/pet-sweep.jpg', 'dog slippers shaped like sweepers');
-const scissors = new Product('scissors', './img/scissors.jpg', 'scissors with a stencil writing out "pizza"');
-const shark = new Product('shark', './img/shark.jpg', 'shark shaped sleeping bag');
-const sweep = new Product('sweep', './img/sweep.png', 'baby onesie with sweepers attached');
-const tauntaun = new Product('tauntaun', './img/tauntaun.jpg', 'tauntaun shaped sleeping bag');
-const unicorn = new Product('unicorn', './img/unicorn.jpg', 'canned unicorn meat');
-const waterCan = new Product('water can', './img/water-can.jpg', 'watering can that shoots water back into itself');
-const wineGlass = new Product('wine glass', './img/wine-glass.jpg', 'egg shaped wine glass with one narrow opening');
+
+
+const bag = new Product('bag', 'img/bag.jpg', 'r2d2 suitcase');
+const banana = new Product('banana', 'img/banana.jpg', 'banana shaped banana slicer');
+const bathroom = new Product('bathroom', 'img/bathroom.jpg', 'toilet paper roll stand that doubles as an ipad stand');
+const boots = new Product('boots', 'img/boots.jpg', 'open toed rain boots');
+const breakfast = new Product('breakfast', 'img/breakfast.jpg', 'a machine that makes eggs, bacon, coffee, and toast all in one');
+const bubblegum = new Product('bubblegum', 'img/bubblegum.jpg', 'meatball bubblegum');
+const chair = new Product('chair', 'img/chair.jpg', 'plastic chair with the seat bubbling outward');
+const cthulhu = new Product('cthulhu', 'img/cthulhu.jpg', 'cthulhu action figure');
+const dogDuck = new Product('dog duck', 'img/dog-duck.jpg', 'dog muzzle shaped like a duck bill');
+const dragon = new Product('dragon', 'img/dragon.jpg', 'canned dragon meat');
+const pen = new Product('pen', 'img/pen.jpg', 'pens with silverware shaped caps');
+const petSweep = new Product('pen sweep', 'img/pet-sweep.jpg', 'dog slippers shaped like sweepers');
+const scissors = new Product('scissors', 'img/scissors.jpg', 'scissors with a stencil writing out "pizza"');
+const shark = new Product('shark', 'img/shark.jpg', 'shark shaped sleeping bag');
+const sweep = new Product('sweep', 'img/sweep.png', 'baby onesie with sweepers attached');
+const tauntaun = new Product('tauntaun', 'img/tauntaun.jpg', 'tauntaun shaped sleeping bag');
+const unicorn = new Product('unicorn', 'img/unicorn.jpg', 'canned unicorn meat');
+const waterCan = new Product('water can', 'img/water-can.jpg', 'watering can that shoots water back into itself');
+const wineGlass = new Product('wine glass', 'img/wine-glass.jpg', 'egg shaped wine glass with one narrow opening');
 
 allProducts = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, waterCan, wineGlass];
 
